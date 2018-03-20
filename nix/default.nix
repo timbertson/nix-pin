e28b3ad2ef2e820973f751d5ed36693262e7d342
Tim Cuthbertson <tim@gfxmonk.net>
2018-03-20 20:59:01 +1100

    add actual sources

--- nix/default.nix
+++ nix/default.nix
@@ -1,27 +1,31 @@
-{ pkgs, stdenv, mypy, python3 }:
-let self = stdenv.mkDerivation {
-	buildInputs = [ python3 mypy ];
-	name = "nix-pin";
-	version = "0.1.0";
-	src = ./local.tgz;
-	buildPhase = ''
-		mypy bin/*
-	'';
-	installPhase = ''
-		mkdir "$out"
-		cp -r bin "$out"
-		cp -r share "$out"
-	'';
-	passthru = {
-		callWithPins = path: { home ? builtins.getEnv "HOME", ... } @ args:
-			let
-				callArgs = removeAttrs args ["home"];
-				pinConfig = /. + "${home}/.config/nix-pin/pins.nix";
-			in
-			import "${self}/share/nix/run.nix" {
-				inherit pkgs pinConfig;
-				pinPath = path;
-				callArgs = args;
-			};
-	};
+{ pkgs, stdenv, fetchFromGitHub, mypy, python3 }:
+let self = stdenv.mkDerivation rec {
+  buildInputs = [ python3 mypy ];
+  name = "nix-pin-${version}";
+  version = "0.1.0";
+  src = fetchFromGitHub {
+    owner = "timbertson";
+    repo = "nix-pin";
+    rev = "version-0.1.0";
+    sha256 = "009hr9pckprpj2j1ighjc8956ijiy9bb8dyf3vgxyqzkyafn83s3";
+  };
+  buildPhase = ''
+    mypy bin/*
+  '';
+  installPhase = ''
+    mkdir "$out"
+    cp -r bin share "$out"
+  '';
+  passthru = {
+    callWithPins = path: { home ? builtins.getEnv "HOME", ... } @ args:
+      let
+        callArgs = removeAttrs args ["home"];
+        pinConfig = /. + "${home}/.config/nix-pin/pins.nix";
+      in
+      import "${self}/share/nix/run.nix" {
+        inherit pkgs pinConfig;
+        pinPath = path;
+        callArgs = args;
+      };
+  };
 }; in self
