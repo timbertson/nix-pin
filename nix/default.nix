c1c9e7c31b49ccd20a2952c8cab92602180e9f22
Tim Cuthbertson <tim@gfxmonk.net>
2018-04-05 13:34:05 +1000

    version bump

--- nix/default.nix
+++ nix/default.nix
@@ -1,12 +1,12 @@
 { pkgs, stdenv, fetchFromGitHub, mypy, python3 }:
 let self = stdenv.mkDerivation rec {
   name = "nix-pin-${version}";
-  version = "0.1.1";
+  version = "0.1.2";
   src = fetchFromGitHub {
     owner = "timbertson";
     repo = "nix-pin";
-    rev = "version-0.1.1";
-    sha256 = "01yhq1n5rc5y5qfwapnzi3dmg2pr34qmvylb8v59rddb9wszd8l4";
+    rev = "version-0.1.2";
+    sha256 = "1zwfb5198qzbjwivgiaxbwva9frgrrqaj92nw8vz95yi08pijssh";
   };
   buildInputs = [ python3 mypy ];
   buildPhase = ''
